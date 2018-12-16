5c62791cbb04fd058f57e97de9b3d86c2ea19440
Gabriel Taylor Russ <gabriel.taylor.russ@gmail.com>
2018-12-16 15:13:02 -0600

    Add Githooks
    Format code

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
 extern crate actix;
 extern crate actix_web;
-extern crate env_logger;
 extern crate clap;
+extern crate env_logger;
 
-use std::sync::Arc;
-use clap::{Arg, ArgMatches};
-use clap::App as Clap;
 use actix_web::{fs, middleware, server, App};
+use clap::App as Clap;
+use clap::{Arg, ArgMatches};
+use std::sync::Arc;
 
 fn main() {
     ::std::env::set_var("RUST_LOG", "actix_web=info");
     ::std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
 
-    let config = get_config();
-    let config = Arc::new(config);
+    let config = Arc::new(get_config());
     let thread_config = config.clone();
     let port = config.value_of("port").unwrap();
     let address = format!("127.0.0.1:{}", port);
@@ -27,41 +26,50 @@ fn main() {
 
         App::new()
             // enable logger
-            .middleware(middleware::Logger::new(r#"%a "%r" %s %b "%{Referer}i" "%{User-Agent}i" %Dms"#))
+            .middleware(middleware::Logger::new(
+                r#"%a "%r" %s %b "%{Referer}i" "%{User-Agent}i" %Dms"#,
+            ))
             .handler(
                 "/",
-                fs::StaticFiles::new(directory).unwrap().index_file(file)
+                fs::StaticFiles::new(directory).unwrap().index_file(file),
             )
-    }).bind(&address)
-        .expect("Cannot start server on given IP/Port")
-        .start();
+    })
+    .bind(&address)
+    .expect("Cannot start server on given IP/Port")
+    .start();
 
     println!("Started http server: {}", &address);
     let _ = sys.run();
 }
 
 fn get_config() -> ArgMatches<'static> {
-    return Clap::new("Statik")
-                  .version("0.1.0")
-                  .author("Gabriel Taylor Russ <gabriel.taylor.russ@gmail.com>")
-                  .about("A simple static file server")
-                  .arg(Arg::with_name("port")
-                       .short("p")
-                       .long("port")
-                       .required(true)
-                       .help("Sets the port of the web server")
-                       .env("PORT"))
-                  .arg(Arg::with_name("directory")
-                       .short("d")
-                       .long("directory")
-                       .required(true)
-                       .help("Sets directory of the static files")
-                       .env("DIRECTORY"))
-                  .arg(Arg::with_name("file")
-                       .short("f")
-                       .long("file")
-                       .help("Specifies the index file name")
-                       .env("FILE")
-                       .default_value("index.html"))
-                  .get_matches()
+    Clap::new("Statik")
+        .version("0.1.0")
+        .author("Gabriel Taylor Russ <gabriel.taylor.russ@gmail.com>")
+        .about("A simple static file server")
+        .arg(
+            Arg::with_name("port")
+                .short("p")
+                .long("port")
+                .required(true)
+                .help("Sets the port of the web server")
+                .env("PORT"),
+        )
+        .arg(
+            Arg::with_name("directory")
+                .short("d")
+                .long("directory")
+                .required(true)
+                .help("Sets directory of the static files")
+                .env("DIRECTORY"),
+        )
+        .arg(
+            Arg::with_name("file")
+                .short("f")
+                .long("file")
+                .help("Specifies the index file name")
+                .env("FILE")
+                .default_value("index.html"),
+        )
+        .get_matches()
 }
